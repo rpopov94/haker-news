2048feea84e5c53ff4a2d9161e47d5ac545e469a
rpopov <rpopov@web-bee.ru>
2022-07-20 12:24:27 +0300

    теперь должно выдаваться 100 записей

--- src/store/action-creators/news.ts
+++ src/store/action-creators/news.ts
@@ -2,15 +2,23 @@ import {Dispatch} from "redux";
 import axios from "axios";
 import {NewIdAction, NewsAction, NewsActionTypes, NewsItems} from "../../types/news_types";
 
+const NUMRER_PAGES = 4;
+
 export const fetchNews = () => {
+    const urls: string[] = [];
     return async (dispatch: Dispatch<NewsAction>) => {
         try {
             dispatch({type: NewsActionTypes.FETCH_NEWS})
-            const response = await axios.get('https://api.hnpwa.com/v0/news/1.json')
-            dispatch({type: NewsActionTypes.FETCH_NEWS_SUCCESS, payload: response.data})
-            setInterval(() => {
-                dispatch({type: NewsActionTypes.FETCH_NEWS_SUCCESS, payload: response.data})
-            }, 60000)
+            for (let i = 1; i <= NUMRER_PAGES; i++) {
+                urls.push(`https://api.hnpwa.com/v0/news/${i}.json`);
+            }
+            const request = urls.map((url) => fetch(url))
+            setTimeout(() =>{
+                Promise.all(request)
+                    .then((responses) => Promise.all(responses.map((response) => response.clone().json())))
+                    .then((jsons) => jsons.forEach((json) => dispatch({type: NewsActionTypes.FETCH_NEWS_SUCCESS, payload: json})));
+                }, 200
+            )
         } catch (e) {
             dispatch({
                 type: NewsActionTypes.FETCH_NEWS_ERROR,
@@ -24,11 +32,11 @@ export const fetchNewsById = (id: string | undefined) => {
     return async (dispatch: Dispatch<NewIdAction>) => {
         try {
             dispatch({type: NewsActionTypes.FETCH_NEWS})
-            const response = await axios.get("https://hacker-news.firebaseio.com/v0/item/" + id + ".json")
-            dispatch({type: NewsActionTypes.FETCH_NEWS_SUCCESS, payload: response.data})
-            setInterval(() => {
-                dispatch({type: NewsActionTypes.FETCH_NEWS_SUCCESS, payload: response.data})
-            }, 60000)
+            setTimeout(() =>{
+                const response = fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+                    .then((response) => response.json())
+                    .then((json) => dispatch({type: NewsActionTypes.FETCH_NEWS_SUCCESS, payload: json}))
+            }, 200)
         } catch (e) {
             dispatch({
                 type: NewsActionTypes.FETCH_NEWS_ERROR,
